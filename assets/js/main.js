a52cb12c1c70e182275b7c52333ab9f2e888923b
Amer Khanfar <abk.sg8@gmail.com>
2022-09-06 16:51:36 +0400

    added services

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,9 +32,15 @@ navLink.forEach((n) => n.addEventListener("click", linkAction));
 /*==================== CHANGE BACKGROUND HEADER ====================*/
 function scrollHeader() {
   const header = document.getElementById("header");
+  const nav = document.getElementById("nav");
   // When the scroll is greater than 100 viewport height, add the scroll-header class to the header tag
-  if (this.scrollY >= 100) header.classList.add("scroll-header");
-  else header.classList.remove("scroll-header");
+  if (this.scrollY >= 100) {
+    header.classList.add("scroll-header");
+    nav.style.backgroundColor = "white";
+  } else {
+    header.classList.remove("scroll-header");
+    nav.style.backgroundColor = "transparent";
+  }
 }
 window.addEventListener("scroll", scrollHeader);
 
@@ -154,48 +160,49 @@ sr.reveal(
 );
 
 /*==================== DARK LIGHT THEME ====================*/
-const themeButton = document.getElementById("theme-button");
-const darkTheme = "dark-theme";
-const iconTheme = "ri-sun-line";
+// const themeButton = document.getElementById("theme-button");
+// const darkTheme = "dark-theme";
+// const iconTheme = "ri-sun-line";
 
 // Previously selected topic (if user selected)
-const selectedTheme = localStorage.getItem("selected-theme");
-const selectedIcon = localStorage.getItem("selected-icon");
+// const selectedTheme = localStorage.getItem("selected-theme");
+// const selectedIcon = localStorage.getItem("selected-icon");
 
 // We obtain the current theme that the interface has by validating the dark-theme class
-const getCurrentTheme = () =>
-  document.body.classList.contains(darkTheme) ? "dark" : "light";
-const getCurrentIcon = () =>
-  themeButton.classList.contains(iconTheme) ? "ri-moon-line" : "ri-sun-line";
+// const getCurrentTheme = () =>
+//   document.body.classList.contains(darkTheme) ? "dark" : "light";
+// const getCurrentIcon = () =>
+//   themeButton.classList.contains(iconTheme) ? "ri-moon-line" : "ri-sun-line";
 
 // We validate if the user previously chose a topic
-if (selectedTheme) {
-  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
-  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
-    darkTheme,
-  );
-  themeButton.classList[selectedIcon === "ri-moon-line" ? "add" : "remove"](
-    iconTheme,
-  );
-}
+// if (selectedTheme) {
+// If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+// document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+//   darkTheme,
+// );
+// themeButton.classList[selectedIcon === "ri-moon-line" ? "add" : "remove"](
+//   iconTheme,
+// );
+// }
 
 // Activate / deactivate the theme manually with the button
-themeButton.addEventListener("click", () => {
-  // Add or remove the dark / icon theme
-  document.body.classList.toggle(darkTheme);
-  themeButton.classList.toggle(iconTheme);
-  // We save the theme and the current icon that the user chose
-  localStorage.setItem("selected-theme", getCurrentTheme());
-  localStorage.setItem("selected-icon", getCurrentIcon());
-});
+// themeButton.addEventListener("click", () => {
+// Add or remove the dark / icon theme
+// document.body.classList.toggle(darkTheme);
+// themeButton.classList.toggle(iconTheme);
+// We save the theme and the current icon that the user chose
+//   localStorage.setItem("selected-theme", getCurrentTheme());
+//   localStorage.setItem("selected-icon", getCurrentIcon());
+// });
 
-/**** mix it up ***********************/
-
-let mixerFeatured = mixitup(".featured__content", {
-  selectors: {
-    target: ".featured__card",
+/**** mix it up ***********************/ let mixerFeatured = mixitup(
+  ".featured__content",
+  {
+    selectors: {
+      target: ".featured__card",
+    },
+    animation: {
+      duration: 300,
+    },
   },
-  animation: {
-    duration: 300,
-  },
-});
+);
